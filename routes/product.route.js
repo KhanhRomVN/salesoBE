3a8411267d442d83cd5ebe0c1b6a18f1d4ff9764
KhanhRomVN <khanhromvn@gmail.com>
2024-06-13 14:19:51 +0700

    update

--- routes/product.route.js
+++ routes/product.route.js
@@ -9,7 +9,7 @@ router.post('/add-product', authToken, productController.addProduct)
 // Lấy toàn bộ Product từ Seller ID
 router.post('/products', authToken, productController.getProductsByUserId)
 // Lấy toàn bộ Product từ Type Product
-router.post('/productstype', authToken, productController.getProductsByType)
+router.post('/productstype', productController.getProductsByType)
 // Lấy Product qua id_product
 router.post('/product', productController.getProductsByProdId)
 
